2bc99c135ae6eb29a4b49de9270efd30537d59de
Miss Natnicha Lertkitjaroenpol <124992406+icecececeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2023-02-10 14:59:31 +0700

    2

--- 1.py
+++ 1.py
@@ -2,3 +2,6 @@ def print_id(student_id):
     print ("student ID = ", student_id)
 if __name__=="__main__":
     print_id("12345")
+def calculate_sum(array):
+    return array.sum()
+calculate_sum(np.array([1,2,3]))
